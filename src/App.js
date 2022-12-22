90f0ee43a5f6aecfcdaad94187eda4b7f65515f6
Santiago Gonzalez <santiagogonzalez597@gmail.com>
2022-12-22 11:52:03 -0300

    nada

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     <div className="App">
       <form>
         <input type='file' name={file} onChange={e => setFile(e.target.files[0])}/>
-        <input type='file' name={file2} onChange={e => setFile2(e.target.files)[1]}/>
+        <input type='file' name={file2} onChange={e => setFile2(e.target.files)[0]}/>
        
         
         <button onClick={handleSubmit}>Enviar</button>
