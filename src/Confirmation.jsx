1d5a970b37bfb88c5fdf60f0b53033bdd4eb53b7
Santiago Gonzalez <santiagogonzalez597@gmail.com>
2022-12-29 10:13:04 -0300

    avanzando

---
+++ src/Confirmation.jsx
@@ -0,0 +1,25 @@
+import  './Confirmation.css'
+import logo from './imagenes/logo.png'
+
+
+
+const Confirmation = () =>{
+
+return (
+
+    <div id='conteiner'>
+        <h1 id="titleC">Formulario enviado con exito!</h1>
+        <img src={logo} alt="logo" id='img1' />
+
+    </div>
+
+
+
+
+)
+
+
+
+}
+
+export default Confirmation
